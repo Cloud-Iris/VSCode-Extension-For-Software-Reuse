0babf29461ae9afc31ca6a9a2e5d69968ec99a61
Cloud-Iris <1661695051@qq.com>
2024-10-13 22:02:01 +0800

    feat: 实现多选框选择

--- src/extension.ts
+++ src/extension.ts
@@ -39,12 +39,51 @@ export function activate(context: vscode.ExtensionContext) {
 
                 case 'openEditorDialog':
                     vscode.window.showInformationMessage('插件成功打开编辑器对话框!');
+                    showWindowQuickPick();
                     break;
             }
         }, undefined, context.subscriptions);
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+async function showWindowQuickPick() {
+    // 选项卡和默认内容
+    const tabOptions: { [key: string]: string } = {
+        'Option 1': 'Default content for Option 1',
+        'Option 2': 'Default content for Option 2',
+        'Option 3': 'Default content for Option 3'
+    };
+
+    // 使用 quick pick 让用户选择一个选项
+    const selectedOption = await vscode.window.showQuickPick(Object.keys(tabOptions), {
+        placeHolder: '选择一个选项卡进行编辑',
+        canPickMany: false
+    });
+
+    if (selectedOption) {
+        // 显示一个输入框让用户编辑内容
+        const userInput = await vscode.window.showInputBox({
+            prompt: `Edit content for ${selectedOption}`,
+            placeHolder: 'Type your content here',
+            value: tabOptions[selectedOption] // 设置默认内容
+        });
+
+        if (userInput) {
+            // 将编辑的内容插入到当前活动的编辑器中
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+                const position = editor.selection.active;
+                editor.edit(editBuilder => {
+                    editBuilder.insert(position, userInput);
+                });
+                vscode.window.showInformationMessage(`${selectedOption} content added to the editor.`);
+            } else {
+                vscode.window.showErrorMessage('No active editor found.');
+            }
+        }
+    }
 }
 
 export function deactivate() {}
