030f3285e8d5eea5eb604f00b3ddd8a69625add0
6643789wsx <qaz6643789wsx@163.com>
2024-10-18 10:43:22 +0800

    feature: 分类处理用户的不同输入

--- src/service/main.py
+++ src/service/main.py
@@ -1,3 +1,20 @@
 from decompose_requirements import decompose_requirements
 if __name__ == "__main__":
-    decompose_requirements("requirements")
+    s=input()
+    # e.g. create: 我想要创建一个计算器
+    # 要和创建其他节点区分开，因为可能会有不同系统
+    if s.beginswith("create"):
+        decompose_requirements("requirements")
+    # e.g. add: 我想要添加所有的计算功能
+    # e.g. add: 我想要在加法里面添加二进制加法功能
+    if s.beginswith("add"):
+        # To Do: 
+        pass
+    # e.g. add: 我想要删除加法功能
+    if s.beginswith("delete"):
+        # To Do: 
+        pass
+    # e.g. add: 我想要把加法功能修改成减法功能
+    if s.beginswith("modify"):
+        # To Do: 
+        pass
