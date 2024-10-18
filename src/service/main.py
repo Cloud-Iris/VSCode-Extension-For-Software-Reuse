ee576bb856e5ab4545720bb6ecdc4e6b20ee9532
6643789wsx <qaz6643789wsx@163.com>
2024-10-18 11:52:47 +0800

    解决需求选择bug

--- src/service/main.py
+++ src/service/main.py
@@ -1,20 +1,21 @@
 from decompose_requirements import decompose_requirements
 if __name__ == "__main__":
-    s=input()
+    print("请输入您的需求：")
+    s = input()
     # e.g. create: 我想要创建一个计算器
     # 要和创建其他节点区分开，因为可能会有不同系统
-    if s.beginswith("create"):
+    if s.startswith("create"):
         decompose_requirements("requirements")
     # e.g. add: 我想要添加所有的计算功能
     # e.g. add: 我想要在加法里面添加二进制加法功能
-    if s.beginswith("add"):
+    if s.startswith("add"):
         # To Do: 
         pass
     # e.g. delete: 我想要删除加法功能
-    if s.beginswith("delete"):
+    if s.startswith("delete"):
         # To Do: 
         pass
     # e.g. modify: 我想要把加法功能修改成减法功能
-    if s.beginswith("modify"):
+    if s.startswith("modify"):
         # To Do: 
         pass
