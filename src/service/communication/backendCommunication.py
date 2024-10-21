98517d87e53d21dece014db7216267ce04a08207
Cloud-Iris <1661695051@qq.com>
2024-10-21 20:23:18 +0800

    feat: 前后端实现初步对接&解决跨域问题

--- src/service/communication/backendCommunication.py
+++ src/service/communication/backendCommunication.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+# 解决跨域问题 pip install Flask-Cors
+from flask_cors import CORS
 from decompose_requirements import RequirementManager
 import os
 from file_system.fileChange import load_tree_from_json
@@ -20,17 +22,36 @@ class BackendService:
         @self.app.route('/health-check', methods=['GET'])
         def health_check():
             return jsonify({'status': 'ok'})
+        
+        @self.app.route('/depose-req-for-level-2', methods=['GET', 'POST'])
+        def depose_req_for_level_2():
+            data:dict = request.get_json()
+            req:str = data.get('req', '')
+            # TODO: 这个是前端传过来的用户原始需求描述
+            # print(req)
 
-        @self.app.after_request
-        def after_request(response):
-            """
-            在请求处理后执行的操作
-            """
-            # 这里可以执行一些请求后的操作
-            manager = RequirementManager(self.parsed_str)
-            manager.user_interaction()
-            os._exit(0)
-            return response
+            # self.parsed_str = data.get('folderPath', '')
+            # print("项目根目录是: "+self.parsed_str)
+            # manager = RequirementManager(self.parsed_str)
+            # manager.user_interaction()
+            _2LevelReqs = [
+                f"默认 2 级需求 111 for {req}",
+                f"默认 2 级需求 222 for {req}",
+                f"默认 2 级需求 333 for {req}"
+            ]
+            return jsonify({'_2LevelReqs': _2LevelReqs})
+
+        # @self.app.after_request
+        # def after_request(response):
+        #     """
+        #     在请求处理后执行的操作
+        #     """
+        #     # 这里可以执行一些请求后的操作
+        #     manager = RequirementManager(self.parsed_str)
+        #     manager.user_interaction()
+        #     os._exit(0)
+        #     return response
 
     def run(self, port=5000):
+        CORS(self.app)  # 解决跨域问题
         self.app.run(port=port)
